8ded47b108a21882d79439d197d2ef825748dbd4
Calvin Ting <calvinting@protonmail.com>
2018-06-03 23:49:37 -0700

    add pod for react-maps

--- App/screens/Map/Map.js
+++ App/screens/Map/Map.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    Button
-} from 'react-native';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
 
 const Map = () => (
     <View>
-        <Text>Hi</Text>
+        {/* <MapView
+            region={{
+                latitude: 49.263474,
+                longitude: -123.13809,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1
+            }}
+        >
+            <MapView.Marker
+                coordinate={{
+                    latitude: 49.263474,
+                    longitude: -123.13809
+                }}
+                title="RED"
+                description="RED"
+            />
+        </MapView> */}
+        <MapView />
     </View>
 );
 
