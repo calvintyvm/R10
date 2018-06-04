33cd352b63cd4c2a7343b207b7fd9ed03543df59
Calvin Ting <calvinting@protonmail.com>
2018-06-04 10:15:27 -0700

    remove all files and revert to working android,ios

--- App/screens/Map/Map.js
+++ App/screens/Map/Map.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
-import MapView from 'react-native-maps';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    Button
+} from 'react-native';
 
 const Map = () => (
     <View>
-        {/* <MapView
-            region={{
-                latitude: 49.263474,
-                longitude: -123.13809,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1
-            }}
-        >
-            <MapView.Marker
-                coordinate={{
-                    latitude: 49.263474,
-                    longitude: -123.13809
-                }}
-                title="RED"
-                description="RED"
-            />
-        </MapView> */}
-        <MapView />
+        <Text>Hi</Text>
     </View>
 );
 
